22c2480ec467760a99037540acdf82dc0ad7526d
Tipiii <112958998+Tipiii@users.noreply.github.com>
2025-06-16 14:31:32 +0700

    up2

--- des_project/des_project/settings.py
+++ des_project/des_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yn&*fikf)8xkrb262v)l))=6^cked)3-v8=&+^b(yunwguh-mc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tipii.pythonanywhere.com']
 
 
 # Application definition
